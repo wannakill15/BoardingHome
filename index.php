151532ee6d61728e9dde1df004980954e9720d63
Mark John <markjohnjopia1@gmail.com>
2024-05-16 04:54:22 +0800

    adding google maps and contacts

--- index.php
+++ index.php
@@ -511,6 +511,60 @@
           </div>
     </div>
 
+    <!-- Our Testemonial form -->
+
+    <h2 class="mt-5 pt-4 mb-4 text-center fw-bold h-font"> Reach Us</h2>
+    
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-8 col-md-8 p-4 mb-lg-0 mb-3 bg-white rounded">
+                <iframe class="w-100 rounded" height="320px" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3966.914850528526!2d125.13251907321018!3d6.142139627481223!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x32f79ecede2b860d%3A0x9c07f8225e462cf1!2sBlock%2003%20Rd%2C%20General%20Santos%20City%2C%20South%20Cotabato!5e0!3m2!1sen!2sph!4v1715803643852!5m2!1sen!2sph" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+            </div>
+            <div class="col-lg-4 rounded col-md-4">
+                <div class="bg-white p-4 rounded md-4">
+                    <h5>Call Us</h5>
+                    <a href="Phone: +639514810354" class="d-inline-block mb-2 text-decoration-none text-dark">
+                        <span class="badge bg-light text-dark fs-6 p-2">
+                            <i class="bi bi-phone-fill"></i> +639514810354
+                        </span>
+                    </a>
+                    <br>
+                    <a href="Phone: +639514810354" class="d-inline-block mb-2 text-decoration-none text-dark">
+                        <span class="badge bg-light text-dark fs-6 p-2">
+                            <i class="bi bi-phone-fill"></i> +639514810354
+                        </span>
+                    </a>
+                    <br>
+                    <a href="Phone: +639514810354" class="d-inline-block mb-2 text-decoration-none text-dark">
+                        <span class="badge bg-light text-dark fs-6 p-2">
+                            <i class="bi bi-phone-fill"></i> +639514810354
+                        </span>
+                    </a>
+                </div>
+                <div class="bg-white p-4 rounded md-4">
+                    <h5>Follow Us</h5>
+                    <a href="#" class="d-inline-block mb-3">
+                        <span class="badge bg-light text-dark fs-6 p-2">
+                            <i class="bi bi-twitter"></i> Twitter
+                        </span>
+                    </a>
+                    <br>
+                    <a href="#" class="d-inline-block mb-3">
+                        <span class="badge bg-light text-dark fs-6 p-2">
+                            <i class="bi bi-facebook"></i> Facebook
+                        </span>
+                    </a>
+                    <br>
+                    <a href="#" class="d-inline-block mb-3">
+                        <span class="badge bg-light text-dark fs-6 p-2">
+                            <i class="bi bi-instagram"></i> Instagram
+                        </span>
+                    </a>
+                </div>
+            </div>
+        </div>
+    </div>
+
     <br><br><br>
     <br><br><br>
 
