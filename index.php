f45b0c117771d0674cd159e0cfed61d42fc93b6e
Mark John <160160952+IamGotu@users.noreply.github.com>
2024-05-18 19:16:07 +0800

    adding footer

--- index.php
+++ index.php
@@ -429,7 +429,7 @@
                 </div>
             </div>
             <div class="col-lg-12 text-center mt-5">
-                <a href="#" class="btn btn-sm btn-outline-dark rounded-0 fw-bold shadow-none">More Rooms>>></a>
+                <a href="#" class="btn btn-sm btn-outline-dark rounded-0 fw-bold shadow-none">More Rooms > > ></a>
             </div>
         </div>
     </div>
@@ -460,7 +460,7 @@
                 <h5 class="mt-3">WiFi</h5>
             </div>
             <div class="col-lg-12 text-center mt-5">
-                <a href="#" class="btn btn-sm btn-outline-dark rounded-0 fw-bold shadow-none">More Facilities>>></a>
+                <a href="#" class="btn btn-sm btn-outline-dark rounded-0 fw-bold shadow-none">More Facilities > > ></a>
             </div>
         </div>
     </div>
@@ -490,8 +490,8 @@
                     <i class="bi bi-star-fill text-warning"></i> 
                     <i class="bi bi-star-fill text-warning"></i>
                 </div>
-              </div>
-              <div class="swiper-slide bg-white p-4">
+                </div>
+                <div class="swiper-slide bg-white p-4">
                 <div class="profile d-flex align-items-center mb-4">
                     <img src="images/features/star.svg" width="30px">
                     <h5 class="m-0 ms-2">Diluc Ragnvindr </h5>
@@ -507,8 +507,8 @@
                     <i class="bi bi-star-fill text-warning"></i>
                    
                 </div>
-              </div>
-              <div class="swiper-slide bg-white p-4">
+                </div>
+                <div class="swiper-slide bg-white p-4">
                 <div class="profile d-flex align-items-center mb-4">
                     <img src="images/features/star.svg" width="30px">
                     <h5 class="m-0 ms-2">Lisa Minci</h5>
@@ -524,14 +524,17 @@
                     <i class="bi bi-star-fill text-warning"></i>
                     <i class="bi bi-star-fill text-warning"></i> 
                 </div>
-              </div>
+            </div>
 
             </div>
             <div class="swiper-pagination"></div>
-          </div>
+        </div>
+        <div class="col-lg-12 text-center mt-5">
+            <a href="#" class="btn btn-sm btn-outline-dark rounded-0 fw-bold shadow-none">Know More > > ></a>
+        </div>
     </div>
 
-    <!-- Our Testemonial form -->
+    <!-- Reach Us -->
 
     <h2 class="mt-5 pt-4 mb-4 text-center fw-bold h-font"> Reach Us</h2>
     
@@ -584,9 +587,37 @@
             </div>
         </div>
     </div>
-
-    <br><br><br>
-    <br><br><br>
+    
+    <div class="container-fluid bg-white mt-5">
+        <div class="row">
+            <div class="col-lg-4 p-4">
+                <h3 class="h-font fw-bold fs-3 mb-2">Boarding Home</h3>
+                <p>
+                    (Discription about the Boarding Home)
+                </p>
+            </div>
+            <div class="col-lg-4 p-4">
+                <h5 class="mb-3">Links</h5>
+                <a href="#" class="d-inline-block mb-2 text-dark text-decoration-none">Home</a> <br>
+                <a href="#" class="d-inline-block mb-2 text-dark text-decoration-none">Rooms</a> <br>
+                <a href="#" class="d-inline-block mb-2 text-dark text-decoration-none">Facilities</a> <br>
+                <a href="#" class="d-inline-block mb-2 text-dark text-decoration-none">Contact Us</a> <br>
+                <a href="#" class="d-inline-block mb-2 text-dark text-decoration-none">About Us</a> <br>
+            </div>
+            <div class="col-lg-4 p-4">
+                <h5 class="mb-3">Follow Us</h5>
+                <a href="#" class="d-inline-block text-dark text-decoration-none mb-2">
+                    <i class="bi bi-twitter"></i> Twitter
+                </a> <br>
+                <a href="#" class="d-inline-block text-dark text-decoration-none mb-2">
+                    <i class="bi bi-facebook"></i> Facebook
+                </a> <br>
+                <a href="#" class="d-inline-block text-dark text-decoration-none">
+                    <i class="bi bi-instagram"></i> Instagram
+                </a> <br>
+            </div>
+        </div>
+    </div>
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.js"></script>
